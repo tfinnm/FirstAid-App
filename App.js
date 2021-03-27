2b8d75677a9aedf80053d994dea6f5e72800ed57
Toby McDonald <toby_mcdonald@email.com>
2021-03-27 16:30:10 -0400

    Update App.js

--- App.js
+++ App.js
@@ -19,30 +19,46 @@ import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 const Root = createStackNavigator();
 
-launcherMenuItems = [
+const launcherMenuItems = [
   {
     id: 1,
     text: "Medical Emergency",
-    page: "med",
-    color : 'red',
+    page: "Medical Emergency | Scene Safety",
+    color: 'red',
+    params: {
+        promptText: "Are you in a safe location? Are you clear of any danger?",
+        yesPage: 'Medical Emergency | Airway/Breathing',
+        noPage: 'Move To Safety',
+        yesParams: {
+
+        },
+        noParams: {
+
+        }
+    },
   },
   {
     id: 2,
     text: "Mental Health Crisis",
-    page: "mh",
-    color : '#344c6c',
+    page: "Mental Health Crisis | C-SSRS 1",
+    color: '#344c6c',
+    params: cssrs1,
   },
   {
     id: 3,
     text: "General Info",
     page: "info",
-    color : '#344c6c',
+    color: '#344c6c',
+    params: {
+
+    },
   },
 ];
 
 const launcher = ({ navigation, route }) => (
   <View style={styles.container}>
     <View style={styles.logoBlock}>
+    {/* this is just a buffer tbh */}
       <Image
         style={[{ height: hp('50%') }, { width: hp('50%') }]}
       />
@@ -53,7 +69,7 @@ const launcher = ({ navigation, route }) => (
       return (
       <TouchableHighlight
       onPress={() => {
-        navigation.push(item.page);
+        navigation.navigate(item.page, item.params);
       }}>
       <View style={[styles.MenuItem,{backgroundColor: item.color,}]}>
         <Text style={[{ color: 'white' }, { fontWeight: 'bold' }]}>
@@ -67,10 +83,20 @@ const launcher = ({ navigation, route }) => (
   </View>
 );
 
-const link1 = ({ navigation, route }) => (
-  <Text style={styles.title}>Screen 2</Text>
-);
-
+function YesNoPrompt({ route, navigation }) {
+  const { promptText } = route.params;
+  const { yesPage } = route.params;
+  const { noPage } = route.params;
+  const { yesParams } = route.params;
+  const { noParams } = route.params;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {JSON.stringify(promptText)}
+      {JSON.stringify(yesPage)}
+      {JSON.stringify(noPage)}
+    </View>
+  );
+}
 export default function App() {
   return (
     <NavigationContainer>
@@ -79,18 +105,25 @@ export default function App() {
         {/* 
           Medical Emergency Screens
         */}
-        <Root.Screen name="Medical Emergency | Scene Safety" component={link1} />
+        <Root.Screen name="Medical Emergency | Scene Safety" component={YesNoPrompt} />
         {/* 
           Mental Health/CSSRS Screesns
         */}
-
+        <Root.Screen name="Mental Health Crisis | C-SSRS 1" component={YesNoPrompt} />
+        <Root.Screen name="Mental Health Crisis | C-SSRS 2" component={YesNoPrompt} />
+        <Root.Screen name="Mental Health Crisis | C-SSRS 3" component={YesNoPrompt} />
+        <Root.Screen name="Mental Health Crisis | C-SSRS 4" component={YesNoPrompt} />
+        <Root.Screen name="Mental Health Crisis | C-SSRS 5" component={YesNoPrompt} />
+        <Root.Screen name="Mental Health Crisis | C-SSRS 6" component={YesNoPrompt} />
+        <Root.Screen name="Mental Health Crisis | Seek Professional Help" component={launcher} />
+        <Root.Screen name="Mental Health Crisis | Seek Immediate Help" component={launcher} />
         {/* 
           General Info Screens
         */}
-        <Root.Screen name="General Info" component={link1} />
-        <Root.Screen name="My Training" component={link1} />
-        <Root.Screen name="Disclaimer" component={link1} />
-        <Root.Screen name="About This App" component={link1} />
+        <Root.Screen name="General Info" component={launcher} />
+        <Root.Screen name="My Training" component={launcher} />
+        <Root.Screen name="Disclaimer" component={launcher} />
+        <Root.Screen name="About This App" component={launcher} />
       </Root.Navigator>
     </NavigationContainer>
   );
@@ -121,3 +154,49 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+
+const cssrs2 = {
+  promptText: 'Ask the patient: "In the last month, have you actually had any thoughts about killing yourself?"',
+  yesPage: 'Mental Health Crisis | C-SSRS 3',
+  noPage: 'Mental Health Crisis | C-SSRS 6',
+  yesParams: cssrs3,
+  noParams: cssrs6
+}
+const cssrs1 = {
+  promptText: 'Ask the patient: "In the last month, have you wished you were dead or wished you could go to sleep and not wake up?"',
+  yesPage: 'Mental Health Crisis | C-SSRS 2',
+  noPage: 'Mental Health Crisis | C-SSRS 2',
+  yesParams: cssrs2,
+  noParams: cssrs2
+}
+const cssrs3 = {
+  promptText: 'Ask the patient: "In the last month, have you though how you might do this?"',
+  yesPage: 'Mental Health Crisis | C-SSRS 4',
+  noPage: 'Mental Health Crisis | C-SSRS 4',
+  yesParams: cssrs4,
+  noParams: cssrs4
+}
+const cssrs4 = {
+  promptText: 'Ask the patient: "In the last month, have you had any intention of acting on these thoughts of killing yourself, as opposed to you have the thoughts but you definately would not act on them?"',
+  yesPage: 'Mental Health Crisis | C-SSRS Seek Immediate Help',
+  noPage: 'Mental Health Crisis | C-SSRS 5',
+  yesParams: {},
+  noParams: cssrs5
+}
+const cssrs5 = {
+  promptText: 'Ask the patient: "In the last month, have you worked out or started to work out the details of how to kill yourself?"',
+  yesPage: 'Mental Health Crisis | C-SSRS Seek Immediate Help',
+  noPage: 'Mental Health Crisis | C-SSRS 6',
+  yesParams: {},
+  noParams: cssrs6
+}
+const cssrs6 = {
+  promptText: 'Ask the patient: "In the past three months, have you done anything, started to do anything, or prepared to do anything to end your life? (For example: collecting pills, self harm, or writing a suicide note)"',
+  yesPage: 'Mental Health Crisis | C-SSRS Seek Immediate Help',
+  noPage: 'Mental Health Crisis | C-SSRS Seek Professional Help',
+  yesParams: {},
+  noParams: {}
+}
